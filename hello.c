9c52295e789790aa5a08c06595d4335a4fc2f152
osha7 <ogroetz@gmail.com>
2024-03-16 23:43:01 -0500

    hello.c file with say/ask/say commands

---
+++ hello.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world");
+}
+
+// #include <cs50.h>
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     string answer = get_string("What's your name? ");
+//     printf("Hello, %s", answer);
+// }
+
+// #include <cs50.h>
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     string answer = get_string("What's your name?\n");
+//     printf("Hello, %s\n", answer);
+// }
